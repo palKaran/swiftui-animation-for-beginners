c9b3ec2586d718fb5152b6852f1b537890529a5d
Karan Pal <palkaran@outlook.com>
2025-07-26 20:34:04 +0530

    Initial Commit

---
+++ SwiftUI Animations/SwiftUI_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationsApp.swift
+//  SwiftUI Animations
+//
+//  Created by Karan Pal on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
